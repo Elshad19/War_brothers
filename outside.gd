44aec1bc2e13a7c1a2d55446da05dc490a60b192
Elshad Abbasov <elshad200131@gmail.com>
2024-11-22 10:56:35 +0400

    Revert "Added an underscore before some unused parameters."
    This reverts commit 40871ac278c79b8b7fbd7a0aeaabd9c5191574d0.

--- outside.gd
+++ outside.gd
@@ -1,10 +1,10 @@
 extends ParentScene
 
-func _on_house_body_entered(_body):
+func _on_house_body_entered(body):
 	var interior_camera_entered = get_tree().create_tween()
 	interior_camera_entered.tween_property($Player/PlayerSprite/Camera2D, "zoom", Vector2(0.6,0.6), 1 )
 
-func _on_house_body_exited(_body):
+func _on_house_body_exited(body):
 	var interior_camera_left = get_tree().create_tween()
 	interior_camera_left.tween_property($Player/PlayerSprite/Camera2D, "zoom", Vector2(0.4,0.4), 1 )
 
